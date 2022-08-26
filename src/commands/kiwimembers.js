a435799c189e0a826a18123e5a4350715f7d8d22
defnot001 <marc.miller994@gmail.com>
2022-08-26 20:43:33 +0200

    caching the guild, please help

--- src/commands/kiwimembers.js
+++ src/commands/kiwimembers.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { guildId } = require('../../config.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('kiwimembers')
     .setDescription('Lists all members of the KiwiTech Minecraft Server.'),
   async execute(interaction) {
-    await interaction.reply('This Does not work yet.');
+    const guild = await interaction.client.guilds.cache.get(guildId);
+    console.log(guild);
   },
 };
