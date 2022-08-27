96fd43dfc395a12356c7a6120cc7456406a0a760
defnot001 <marc.miller994@gmail.com>
2022-08-27 16:13:56 +0200

    made this command functional

--- src/commands/kiwimembers.js
+++ src/commands/kiwimembers.js
@@ -1,12 +1,37 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { guildId } = require('../../config.json');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { memberRoleId } = require('../../config.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('kiwimembers')
     .setDescription('Lists all members of the KiwiTech Minecraft Server.'),
   async execute(interaction) {
-    const guild = await interaction.client.guilds.cache.get(guildId);
-    console.log(guild);
+    try {
+      await interaction.guild.members.fetch();
+
+      const kiwiMembers = await interaction.guild.roles.cache
+        .get(memberRoleId)
+        .members.map((m) => m.user.username)
+        .toString()
+        .replaceAll(',', '\n');
+
+      const KiwiMemberEmbed = await new EmbedBuilder()
+        .setColor('#35aa78')
+        .setAuthor({
+          name: 'defBot',
+          iconURL: interaction.user.client.user.displayAvatarURL(),
+        })
+        .setThumbnail(interaction.guild.iconURL())
+        .addFields({ name: 'KiwiTech Memberlist', value: kiwiMembers })
+        .setTimestamp()
+        .setFooter({
+          text: `Requested by ${interaction.user.username}.`,
+          iconURL: interaction.user.displayAvatarURL(),
+        });
+
+      await interaction.reply({ embeds: [KiwiMemberEmbed] });
+    } catch (err) {
+      console.error(err);
+    }
   },
 };
