b543b84c806f15aeb07941ab994f2cc5da9d432b
defnot001 <marc.miller994@gmail.com>
2022-09-03 22:54:32 +0200

    added working trialinfo command

--- src/commands/trialinfo.js
+++ src/commands/trialinfo.js
@@ -23,7 +23,7 @@ module.exports = {
 
     if (interaction.channelId !== guild.channelIds.memberGeneral) {
       await interaction.reply({
-        content: `You can only use this command in ${guild.channelIds.memberGeneral}`,
+        content: `You can only use this command in <#${guild.channelIds.memberGeneral}>!`,
         ephemeral: true,
       });
       return;
@@ -32,14 +32,31 @@ module.exports = {
 
     const trialEmbed = new EmbedBuilder({
       color: parseInt(embedColor),
-      title: `Trial Info for <@${target.id}>`,
+      title: `${guild.emojis.kiwi}  Welcome to KiwiTech ${target.user.username}!  ${guild.emojis.kiwi}`,
       thumbnail: {
         url: target.user.displayAvatarURL(),
       },
       fields: [
         {
+          name: `${guild.emojis.owoKiwi}  Server Tour`,
+          value:
+            'Please let us know, when you have time for the server tour. Make sure to take your time, it will most likely take around two hours. You will be whitelisted once the tour starts.',
+        },
+        {
+          name: `${guild.emojis.owoKiwi}  Mods & Resources`,
+          value: `On KiwiTech we share our waypoints with a mod that Earthcomputer wrote. You can find it in <#${guild.channelIds.modsResources}>. Also make sure to put the mod for instamineable deepslate in your mods-folder.`,
+        },
+        {
+          name: `${guild.emojis.owoKiwi}  Server Info`,
+          value: `You can find the IPs of our servers in <#${guild.channelIds.serverInfo}>. There is also instructions on how to connect to KiwiTech using KCP. This is especially useful if you live outside of Europe and/or have unstable connection. Make sure to also read the SMP rules.`,
+        },
+        {
+          name: `${guild.emojis.owoKiwi}  Todo on KiwiTech`,
+          value: `When you got your trial member role, we also gave you the Kiwi Inc. role. This role gets pinged from time to time to inform active SMP players about new projects or important things to do on our servers. You can check out our <#${guild.channelIds.todoList}> to see what needs to be done or bring your own ideas and discuss them with us.`,
+        },
+        {
           name: '\u200b',
-          value: 'Message for trial member.',
+          value: `*The most important thing on KiwiTech is to have fun! If you have any questions, you can always ask us anything in member channels or ingame. We are also active in VC!*  ${guild.emojis.froghypers}`,
         },
       ],
       footer: {
@@ -49,6 +66,14 @@ module.exports = {
       timestamp: new Date().toISOString(),
     });
 
-    await interaction.reply({ embeds: [trialEmbed] });
+    await interaction.reply({
+      content: `<@${target.user.id}>`,
+      embeds: [trialEmbed],
+    });
+
+    await interaction.editReply({
+      content: '\u200b',
+      embeds: [trialEmbed],
+    });
   },
 };
