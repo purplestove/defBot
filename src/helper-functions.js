0bf538712ae4e9a3d92dd0cbef043c31876ceab6
defnot001 <marc.miller994@gmail.com>
2022-09-08 19:35:57 +0200

    added query function for mspt via rcon

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -18,3 +18,30 @@ exports.buildDefaultEmbed = (user) =>
     },
     timestamp: Date.now(),
   });
+
+exports.queryMSPT = async (host, port, rconPassword) => {
+  const options = { timeout: 1000 * 5 };
+  const rcon = new util.RCON();
+
+  await rcon.connect(host, port, options);
+  await rcon.login(rconPassword, options);
+
+  const data = await rcon.execute(
+    `script run reduce(system_info('server_last_tick_times'), _a+_, 0)/100`
+  );
+
+  await rcon.close();
+
+  const mspt = (Math.round(parseFloat(data.split(' ')[2]) * 100) / 100).toFixed(
+    2
+  );
+  let tps;
+
+  if (mspt <= 50) {
+    tps = '20.0';
+  } else {
+    tps = 1000 / mspt;
+  }
+
+  return { mspt, tps };
+};
